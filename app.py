67fd0b1759d752146c309078b2582f58139f7649
Дарья Бакалец <dariabakalets@MacBook-Air-Dara-3.local>
2023-05-23 11:35:22 +0300

    Added predict price method

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
-from flask import Flask
+from flask import Flask, request
+import joblib
+import numpy
+
+MODEL_PATH = 'mlmodels/model.pkl'
 
 app = Flask(__name__)
+model = joblib.load(MODEL_PATH)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/predict_price', methods = ['GET'])
+def predict():
+    args = request.args
+    open_plan = args.get('open_plan', default=-1, type=int)
+    rooms = args.get('rooms', default=-1, type=int)
+    area = args.get('area', default=-1, type=float)
+    renovation = args.get('renovation', default=-1, type=float)
+    floor = args.get('floor', default=-1, type=int)
+    studio = args.get('studio', default=-1, type=int)
+    offer_time = args.get('offer_time', default=-1, type=int)
+    agent_fee = args.get('agent_fee', default=-1, type=float)
+
+    x = numpy.array([open_plan, rooms, area, renovation, floor, studio, offer_time, agent_fee]).reshape(1, -1)
+    result = model.predict(x)
+
+    return str(result[0][0])
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=5444, host='0.0.0.0')
+
